bc1fdb3c7f6ef106c7bc7187aec0f4b50cd0df3b
Muhammad Mustafa <mustafa9140@gmail.com>
2022-03-25 14:57:37 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     headers: {
         "Content-Type": "application/json",
-        Authorization: "bearer " + "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",
+        Authorization: "bearer " + "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",
     },
 });
 
